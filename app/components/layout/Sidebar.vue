f845d1dc8082771a6e5863a648ae024e211ccce6
euphoo-01 <euphoo86@gmail.com>
2026-04-07 14:58:45 +0300

    feat(layout): sidebar adaptive for mobile

--- app/components/layout/Sidebar.vue
+++ app/components/layout/Sidebar.vue
@@ -1,9 +1,134 @@
+<template>
+  <v-navigation-drawer
+    permanent
+    :location="isMobile ? 'bottom' : 'left'"
+    :width="isMobile ? undefined : 250"
+    :height="isMobile ? 64 : undefined"
+    :rail="isMobileRail"
+    :class="[
+      'border-0 bg-surface flex transition-all duration-200',
+      isMobile ? 'w-full overflow-hidden' : 'flex-column pt-5 h-screen',
+    ]"
+  >
+    <v-list
+      nav
+      density="comfortable"
+      :class="[
+        'transition-all duration-200',
+        isMobile
+          ? 'flex flex-row justify-evenly items-center w-full pa-0 h-full overflow-x-auto no-scrollbar px-1 gap-1'
+          : 'pt-8',
+        isMobileRail && !isMobile ? 'px-2' : 'pe-4 ps-0',
+      ]"
+    >
+      <template v-for="item in menuItems" :key="item.value">
+        <v-divider v-if="item.divider && !isMobile" class="my-2"></v-divider>
+
+        <v-list-item
+          v-else-if="!item.divider"
+          :to="item.to"
+          :title="
+            item.titleKey && !isMobileRail ? $t(item.titleKey) : undefined
+          "
+          :value="item.value"
+          :active="item.to ? isItemActive(item.to) : false"
+          variant="text"
+          active-class="bg-secondary font-weight-bold"
+          :class="[
+            'text-on-background transition-all duration-200',
+            isMobile
+              ? '!min-w-[40px] !min-h-[40px] !w-[40px] !h-[40px] !p-0 !m-0 flex items-center justify-center shrink-0 mobile-center-item'
+              : 'mb-2',
+            isMobileRail ? 'rounded-full' : 'rounded-r-full rounded-l-0',
+          ]"
+          :data-test-id="`sidebar-item-${item.value}`"
+        >
+          <template v-if="!isMobile" #prepend>
+            <v-icon :icon="item.icon" class="m-0"></v-icon>
+          </template>
+          <template v-if="isMobile">
+            <v-icon :icon="item.icon" size="22" class="ma-0"></v-icon>
+          </template>
+        </v-list-item>
+      </template>
+
+      <v-list-item
+        v-if="isMobile && authStore?.userId"
+        :to="`/users/${authStore.userId}/profile`"
+        :class="[
+          'transition-all duration-200 mobile-center-item',
+          '!min-w-[40px] !min-h-[40px] !w-[40px] !h-[40px] !p-0 !m-0 rounded-full flex items-center justify-center shrink-0',
+        ]"
+        data-test-id="sidebar-profile-link-mobile"
+      >
+        <v-avatar color="primary" size="30" class="ma-0">
+          <v-img
+            v-if="authStore?.user?.profile?.avatar"
+            :src="authStore.user.profile.avatar"
+          ></v-img>
+          <span v-else class="text-caption text-on-primary">
+            {{ authStore?.user?.email?.charAt(0).toUpperCase() || 'U' }}
+          </span>
+        </v-avatar>
+      </v-list-item>
+    </v-list>
+
+    <template v-if="!isMobile" #append>
+      <v-list nav density="comfortable" class="bg-transparent pa-2">
+        <v-list-item
+          v-if="authStore?.userId"
+          variant="text"
+          min-height="64"
+          :class="[isMobileRail ? 'px-0' : 'px-4', 'text-none rounded-lg']"
+          :to="`/users/${authStore.userId}/profile`"
+          data-test-id="sidebar-profile-link"
+        >
+          <template #prepend>
+            <v-avatar color="primary" size="40">
+              <v-img
+                v-if="authStore?.user?.profile?.avatar"
+                :src="authStore.user.profile.avatar"
+              ></v-img>
+              <span v-else class="text-h6 text-on-primary">
+                {{ authStore?.user?.email?.charAt(0).toUpperCase() || 'U' }}
+              </span>
+            </v-avatar>
+          </template>
+          <v-list-item-title
+            v-if="!isMobileRail"
+            class="text-body-1 font-weight-medium"
+          >
+            {{ authStore?.user?.profile?.first_name || 'User' }}
+          </v-list-item-title>
+        </v-list-item>
+      </v-list>
+
+      <div
+        class="px-2 pb-3 mt-1 d-flex"
+        :class="isMobileRail ? 'justify-center' : 'justify-end'"
+      >
+        <v-btn variant="text" icon size="small" @click="isRail = !isRail">
+          <v-icon size="24">{{
+            isMobileRail ? 'mdi-chevron-right' : 'mdi-chevron-left'
+          }}</v-icon>
+        </v-btn>
+      </div>
+    </template>
+  </v-navigation-drawer>
+</template>
+
 <script setup lang="ts">
-import { ref } from 'vue';
+import { ref, computed } from 'vue';
+import { useDisplay } from 'vuetify';
 
 const authStore = useAuthStore();
 const route = useRoute();
+const { width } = useDisplay();
+
+const isMobile = computed(() => width.value <= 768);
 const isRail = ref(false);
+
+const isMobileRail = computed(() => isMobile.value || isRail.value);
 
 const isItemActive = (path: string) => {
   if (path === '/') return route.path === '/';
@@ -29,10 +154,7 @@ const menuItems = [
     titleKey: 'sidebar.cvs',
     value: 'cvs',
   },
-  {
-    divider: true,
-    value: 'divider-1',
-  },
+  { divider: true, value: 'divider-1' },
   {
     to: '/departments',
     icon: 'mdi-domain',
@@ -60,97 +182,24 @@ const menuItems = [
 ];
 </script>
 
-<template>
-  <v-navigation-drawer
-    permanent
-    :width="250"
-    :rail="isRail"
-    class="border-0 bg-surface flex flex-column pt-5 h-screen"
-  >
-    <v-list
-      class="pt-8 transition-all duration-200"
-      :class="isRail ? 'px-2' : 'pe-4 ps-0'"
-      nav
-      density="comfortable"
-    >
-      <template v-for="item in menuItems" :key="item.value">
-        <v-divider v-if="item.divider" class="my-2"></v-divider>
-        <v-list-item
-          v-else
-          :to="item.to"
-          :prepend-icon="item.icon"
-          :title="item.titleKey ? $t(item.titleKey) : undefined"
-          :value="item.value"
-          :active="item.to ? isItemActive(item.to) : false"
-          variant="text"
-          density="default"
-          active-class="bg-secondary font-weight-bold"
-          class="text-on-background"
-          :class="[
-            'mb-2 transition-all duration-200',
-            isRail ? 'rounded-full' : 'rounded-r-full rounded-l-0',
-          ]"
-        ></v-list-item>
-      </template>
-    </v-list>
+<style scoped>
+:deep(.v-list-item__prepend) {
+  margin-inline-end: 0 !important;
+}
 
-    <template #append>
-      <v-list
-        nav
-        density="comfortable"
-        class="bg-transparent pa-2 transition-all duration-200"
-      >
-        <v-list-item
-          variant="text"
-          min-height="64"
-          :class="[
-            isRail ? 'px-0' : 'px-4',
-            'text-none rounded-lg transition-all duration-200',
-          ]"
-          :to="`/users/${authStore.userId}/profile`"
-        >
-          <template #prepend>
-            <v-avatar color="primary" size="40">
-              <v-img
-                v-if="authStore.user?.profile?.avatar"
-                :src="authStore.user?.profile?.avatar"
-                alt="Avatar"
-              ></v-img>
-              <span v-else class="text-h6 text-on-primary">{{
-                authStore.user?.email?.charAt(0).toUpperCase()
-              }}</span>
-            </v-avatar>
-          </template>
-          <v-list-item-title
-            v-if="!isRail"
-            class="text-body-1 font-weight-medium"
-          >
-            {{
-              authStore.user?.profile?.first_name ||
-              authStore.user?.profile?.last_name
-                ? `${authStore.user?.profile?.first_name || ''} ${authStore.user?.profile?.last_name || ''}`.trim()
-                : 'User'
-            }}
-          </v-list-item-title>
-        </v-list-item>
-      </v-list>
+.no-scrollbar {
+  -ms-overflow-style: none;
+  scrollbar-width: none;
+}
+.no-scrollbar::-webkit-scrollbar {
+  display: none;
+}
 
-      <div
-        class="px-2 pb-3 mt-1 d-flex"
-        :class="isRail ? 'justify-center' : 'justify-end'"
-      >
-        <v-btn
-          variant="text"
-          icon
-          size="small"
-          class="transition-all duration-200"
-          @click="isRail = !isRail"
-        >
-          <v-icon size="24">{{
-            isRail ? 'mdi-chevron-right' : 'mdi-chevron-left'
-          }}</v-icon>
-        </v-btn>
-      </div>
-    </template>
-  </v-navigation-drawer>
-</template>
+.mobile-center-item :deep(.v-list-item__content) {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 100%;
+}
+</style>
